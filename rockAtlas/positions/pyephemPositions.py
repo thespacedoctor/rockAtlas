d3a058cbc9f40a852f566a73041066e13aead7f9
thespacedoctor <nothingbutdave@gmail.com>
2017-11-13 13:48:06 +0000

    x

--- rockAtlas/positions/pyephemPositions.py
+++ rockAtlas/positions/pyephemPositions.py
@@ -370,6 +370,19 @@ class pyephemPositions():
             dbConn=self.atlasMoversDBConn,
         )
 
+        sqlQuery = """update 
+            atlas_exposures
+        set orbfit_positions = 1
+        WHERE
+            pyephem_positions = 1
+                AND orbfit_positions = 0 and expname not in (select distinct expname from pyephem_positions);""" % locals(
+        )
+        writequery(
+            log=self.log,
+            sqlQuery=sqlQuery,
+            dbConn=self.atlasMoversDBConn,
+        )
+
         self.log.info('completed the ``_update_database_flag`` method')
         return None
 
