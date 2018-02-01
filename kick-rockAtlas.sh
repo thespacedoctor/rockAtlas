1d5d3ada19f6b1a78907aacb1efacfc6a848bd41
David Young <davidrobertyoung@gmail.com>
2018-02-01 14:49:33 +0000

    rockAtlas kick start script working

--- kick-rockAtlas.sh
+++ kick-rockAtlas.sh
@@ -2,7 +2,8 @@
 ps_out=`ps -ef | grep rockAtlas | grep -v 'grep' | grep -v $0`
 result=$(echo $ps_out | grep "rockAtlas")
 if [[ "$result" != "" ]];then
-    echo "Running"
+    echo "rockAtlas already running - moving on"
 else
-    echo "Not Running"
+    echo "rockAtlas not running - kick starting"i
+    /home/dry/.conda/envs/rockAtlas/bin/rockAtlas cycle 5
 fi
