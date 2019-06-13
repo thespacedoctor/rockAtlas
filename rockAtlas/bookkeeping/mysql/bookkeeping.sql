b9385ce350cad4d25343f75fa2b1575531bd9e4f
thespacedoctor <nothingbutdave@gmail.com>
2019-06-13 10:24:47 +0100

    commenting out update of atlas_object -- takes long time

--- rockAtlas/bookkeeping/mysql/bookkeeping.sql
+++ rockAtlas/bookkeeping/mysql/bookkeeping.sql
@@ -2,4 +2,4 @@ update atlas_exposures set pyephem_mjd = ROUND(mjd, 1) where pyephem_mjd is null
 CALL update_dophot_photometry();
 CALL update_stats_exposure_processing();
 CALL update_stats_objects();
-CALL update_atlas_objects();
+-- CALL update_atlas_objects();
